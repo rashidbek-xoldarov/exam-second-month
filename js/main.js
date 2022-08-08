b6b3312f993b86cab66c8afa24752c0226c91e44
rashid1111111 <rashidbek0309@gmail.com>
2022-08-08 18:51:45 +0500

    Add open page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const elModalOpenBtn = document.querySelector(".ask-question-btn");
 const elModal = document.querySelector(".modal");
 const elModalCloseBtn = document.querySelector(".modal__close");
+// const elLinkBtn = document.querySelectorAll(".item-intro__link");
+// const elLinkTitle = document.querySelectorAll(".item-intro__title");
+// const elTitleNew = document.querySelector(".info-open__title");
 
 elModalOpenBtn.addEventListener("click", function () {
   elModal.classList.add("modal-open");
@@ -14,4 +17,15 @@ elModal.addEventListener("click", function () {
   elModal.classList.remove("modal-open");
 });
 
-console.log(elModalOpenBtn);
+// elLinkBtn.forEach((item, index) => {
+//   item.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     location.href = "./open.html";
+//     // window.stop();
+//     elLinkTitle.forEach((title) => {
+//       if (index == title.getAttribute("dataset")) {
+//         arr.push(title.innerHTML);
+//       }
+//     });
+//   });
+// });
